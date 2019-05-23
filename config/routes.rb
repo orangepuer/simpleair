57f4636f20c547a1d3d25cad6d1a344298e84a93
orangepuer <rkhpuer@gmail.com>
2019-05-23 16:06:08 +0300

    Add edit profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  devise_for :users
+  devise_for :users, path_names: { sign_in: 'login', edit: 'edit/profile' }
 
   resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
