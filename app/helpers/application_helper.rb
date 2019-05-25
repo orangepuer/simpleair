71749f081213f3a845d336a9fd57928ac14b30e2
orangepuer <rkhpuer@gmail.com>
2019-05-25 22:02:28 +0300

    Add flash_error_messages helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
     "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=retro"
   end
+
+  def flash_error_messages(resource)
+    flash['error_messages'] = []
+
+    resource.errors.full_messages.each do |message|
+      flash['error_messages'] << message
+    end
+  end
 end
