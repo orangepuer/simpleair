eba9323a5d0d2f862e7e2e932ee0fd909be55f6e
orangepuer <rkhpuer@gmail.com>
2019-06-04 21:28:20 +0300

    Add photos to Room model using active storage

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,7 @@
 class Room < ApplicationRecord
   belongs_to :user
 
+  has_many_attached :photos
+
   validates :home_type, :room_type, :accommodate, :bedroom_amount, :bathroom_amount, presence: true
 end
