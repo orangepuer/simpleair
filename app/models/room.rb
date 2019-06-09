2289f605890f9a6acceb1247ee7381950e69f874
orangepuer <rkhpuer@gmail.com>
2019-06-09 21:53:20 +0300

    Add data_prepared? method to Room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,4 +4,8 @@ class Room < ApplicationRecord
   has_many_attached :photos
 
   validates :home_type, :room_type, :accommodate, :bedroom_amount, :bathroom_amount, presence: true
+
+  def data_prepared?
+    listing_name.present? && address.present? && price.present? && photos.attached?
+  end
 end
