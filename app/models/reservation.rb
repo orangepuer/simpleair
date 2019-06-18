303fa59062c6009c2c2e5b4cb1e68ef0af18b097
orangepuer <rkhpuer@gmail.com>
2019-06-18 20:59:42 +0300

    Add validates, methods to Reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,29 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :room
+
+  validates :start_date, :end_date, presence: true
+  validate :end_date_more, if: Proc.new { |a| a.start_date.present? && a.end_date.present? }
+
+  before_save :set_price, :set_total
+
+  private
+
+  def days_count
+    @days_count ||= end_date.to_date - start_date.to_date + 1
+  end
+
+  def end_date_more
+    if days_count < 1
+      errors.add(:base, 'End date should be more of start date')
+    end
+  end
+
+  def set_price
+    self.price = room.price
+  end
+
+  def set_total
+    self.total = price * days_count
+  end
 end
--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 RSpec.describe Reservation, type: :model do
   it { should belong_to :user }
   it { should belong_to :room }
+
+  it { should validate_presence_of :start_date }
+  it { should validate_presence_of :end_date }
 end
