a99fb4b69813b6c4d901501a43d40a75efd7575a
orangepuer <rkhpuer@gmail.com>
2019-06-20 20:58:44 +0300

    Update rooms.js, move code from reservations/_form partial

--- app/assets/javascripts/rooms.js
+++ app/assets/javascripts/rooms.js
@@ -4,10 +4,39 @@ function initMap(lat, lng) {
     center: myCoords,
     zoom: 14
   };
-  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+  var googleMap = document.getElementById('map');
+
+  if (googleMap) {
+    var map = new google.maps.Map(googleMap, mapOptions);
+  }
 
   var marker = new google.maps.Marker({
     position: myCoords,
     map: map
   });
 }
+
+document.addEventListener("turbolinks:load", function() {
+  $(function() {
+    $('#reservation_start_date').datepicker({
+      dateFormat: 'dd-mm-yy'
+    });
+
+    $('#reservation_end_date').datepicker({
+      dateFormat: 'dd-mm-yy'
+    });
+  });
+});
+
+document.addEventListener("turbolinks:before-cache", function() {
+  $.datepicker.dpDiv.remove();
+
+  for (let element of Array.from(document.querySelectorAll("input.hasDatepicker"))) {
+    $(element).datepicker("destroy");
+  }
+});
+
+document.addEventListener("turbolinks:before-render", function() {
+  $.datepicker.dpDiv.appendTo(event.data.newBody);
+});
