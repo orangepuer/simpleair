a6770e02d0852ad5b3e1495c9b3f7f2454b8fd30
orangepuer <rkhpuer@gmail.com>
2019-06-22 22:50:51 +0300

    Implement the display of reserved dates in the reservation calendar

--- app/assets/javascripts/rooms.js
+++ app/assets/javascripts/rooms.js
@@ -17,17 +17,42 @@ function initMap(lat, lng) {
   });
 }
 
-document.addEventListener("turbolinks:load", function() {
-  $(function() {
-    $('#reservation_start_date').datepicker({
-      dateFormat: 'dd-mm-yy'
-    });
+function initCalendar(reservedDates) {
+  let unavailableDates = [];
 
-    $('#reservation_end_date').datepicker({
-      dateFormat: 'dd-mm-yy'
-    });
+  function checkDate(date) {
+    dayMonthYear = date.getDate() +  '-' + (date.getMonth() + 1) + '-' + date.getFullYear();
+
+    return [!unavailableDates.includes(dayMonthYear)]
+  }
+
+  reservedDates.forEach(function (value) {
+    for(let day = new Date(value.start_date); day <= new Date(value.end_date); day.setDate(day.getDate() + 1)) {
+      unavailableDates.push($.datepicker.formatDate('d-m-yy', day));
+    }
   });
-});
+
+  $('#reservation_start_date').datepicker({
+    dateFormat: 'dd-mm-yy',
+    minDate: 0,
+    maxDate: '3m',
+    beforeShowDay: checkDate,
+    onSelect: function (selected) {
+      $('#reservation_end_date').datepicker('option', 'minDate', selected);
+      $('#reservation_end_date').attr('disabled', false);
+    }
+  });
+
+  $('#reservation_end_date').datepicker({
+    dateFormat: 'dd-mm-yy',
+    minDate: 0,
+    maxDate: '3m',
+    beforeShowDay: checkDate,
+    onSelect: function (selected) {
+      $('#reservation_start_date').datepicker('option', 'maxDate', selected);
+    }
+  });
+}
 
 document.addEventListener("turbolinks:before-cache", function() {
   $.datepicker.dpDiv.remove();
