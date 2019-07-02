ee2c8faad878b46a2db7ad750d597fe466f6bbf9
orangepuer <rkhpuer@gmail.com>
2019-07-02 11:22:49 +0300

    Add HostReviewsController

---
+++ app/controllers/host_reviews_controller.rb
@@ -0,0 +1,38 @@
+class HostReviewsController < ApplicationController
+  before_action :set_reservation, only: [:create]
+  before_action :authenticate_user!
+
+  def create
+    if @reservation.host_review.blank?
+      host_review = current_user.host_reviews.new(host_review_params)
+
+      if host_review.save
+        flash[:notice] = 'Review created'
+      else
+        helpers.flash_error_messages(host_review)
+      end
+    else
+      flash[:alert] = 'You have already made a review'
+    end
+
+    redirect_back(fallback_location: request.referer)
+  end
+
+  def destroy
+    host_review = HostReview.find(params[:id])
+    host_review.destroy
+
+    redirect_back(fallback_location: request.referer, notice: 'Removed')
+  end
+
+
+  private
+
+  def set_reservation
+    @reservation = Reservation.find(host_review_params[:reservation_id])
+  end
+
+  def host_review_params
+    params.require(:host_review).permit(:comment, :star, :reservation_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
     resources :reservations, only: [:create]
   end
 
+  resources :host_reviews, only: [:create, :destroy]
+
   get 'your_trips', to: 'reservations#your_trips'
   get 'your_reservations', to: 'reservations#your_reservations'
 
