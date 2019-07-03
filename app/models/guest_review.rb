225d8e70b03992249334acc8ac4e64c7caae8570
orangepuer <rkhpuer@gmail.com>
2019-07-03 12:23:53 +0300

    Add callbacks to GuestReview model

--- app/models/guest_review.rb
+++ app/models/guest_review.rb
@@ -1,3 +1,15 @@
 class GuestReview < Review
   belongs_to :guest, class_name: 'User'
+
+  before_validation :set_room, :set_host, if: Proc.new { |a| a.reservation.present? }
+
+  private
+
+  def set_room
+    self.room = reservation.room
+  end
+
+  def set_host
+    self.host_id = reservation.room.user.id
+  end
 end
