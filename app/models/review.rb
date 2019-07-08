b0f90e1077b2485105ac36ce28c7ec13deee4004
orangepuer <rkhpuer@gmail.com>
2019-07-08 14:48:19 +0300

    Add validates to Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :room
   belongs_to :reservation
+
+  validates :star, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,4 +3,8 @@ require 'rails_helper'
 RSpec.describe Review, type: :model do
   it { should belong_to :room }
   it { should belong_to :reservation }
+
+  it { should validate_numericality_of(:star).only_integer }
+  it { should validate_numericality_of(:star).is_greater_than_or_equal_to(1) }
+  it { should validate_numericality_of(:star).is_less_than_or_equal_to(5) }
 end
