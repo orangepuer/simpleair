3e18fb0e363794d9ca4a7ad16e90a4d01a90d336
orangepuer <rkhpuer@gmail.com>
2019-07-29 15:44:34 +0300

    Add details of room to home page, user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @rooms = @user.rooms
     @guest_reviews = GuestReview.where(host_id: @user)
     @host_reviews = HostReview.where(guest_id: @user)
   end
