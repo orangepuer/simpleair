9a144917654c957cd453676496a97206e5622781
orangepuer <rkhpuer@gmail.com>
2019-08-11 17:13:16 +0300

    Add test for Room#available?

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe Room, type: :model do
   it { should validate_presence_of :accommodate }
   it { should validate_presence_of :bedroom_amount }
   it { should validate_presence_of :bathroom_amount }
+
+  describe 'available?' do
+    let(:user) { create(:user) }
+    let(:another_user) { create(:user) }
+    let(:room) { create(:room, user: user) }
+    let!(:reservation) { create(:reservation, user: another_user, room: room,
+                                start_date: '2019-08-07', end_date: '2019-08-10') }
+
+    it 'Room should be available' do
+      expect(room).to be_available('2019-08-11', '2019-08-15')
+    end
+
+    it 'Room does not available' do
+      expect(room).to_not be_available('2019-08-06', '2019-08-11')
+      expect(room).to_not be_available('2019-08-08', '2019-08-09')
+    end
+  end
 end
