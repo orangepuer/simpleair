e42ed2595f5d271e6a533347ff3079bdc49f7aea
orangepuer <rkhpuer@gmail.com>
2019-08-14 12:56:17 +0300

    Update HostReviewsController

--- app/controllers/host_reviews_controller.rb
+++ app/controllers/host_reviews_controller.rb
@@ -1,20 +1,15 @@
 class HostReviewsController < ApplicationController
-  before_action :set_reservation, only: [:create]
   before_action :authenticate_user!
 
   authorize_resource
 
   def create
-    if @reservation.host_review.blank?
-      host_review = current_user.host_reviews.new(host_review_params)
+    host_review = current_user.host_reviews.new(host_review_params)
 
-      if host_review.save
-        flash[:notice] = 'Review created'
-      else
-        helpers.flash_error_messages(host_review)
-      end
+    if host_review.save
+      flash[:notice] = 'Review created'
     else
-      flash[:alert] = 'You have already made a review'
+      helpers.flash_error_messages(host_review)
     end
 
     redirect_back(fallback_location: request.referer)
@@ -27,12 +22,7 @@ class HostReviewsController < ApplicationController
     redirect_back(fallback_location: request.referer, notice: 'Removed')
   end
 
-
   private
-
-  def set_reservation
-    @reservation = Reservation.find(host_review_params[:reservation_id])
-  end
 
   def host_review_params
     params.require(:host_review).permit(:comment, :star, :reservation_id)
